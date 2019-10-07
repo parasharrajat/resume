ff23682b16f9f0209f09432b6f2ad3e870d18a5a
Rajat Parashar <parasharrajat@users.noreply.github.com>
2019-10-08 04:24:35 +0530

    new: download feature

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 
 $(document).ready(function () {
+
+  // $('[data-toggle="popover"]').popover();
+  // load the correct resume
   let selectedVr = "";
   const vrDrop = $('#vr-drop-btn');
   const resumeFr = $('#resume-frame');
@@ -14,15 +17,42 @@ $(document).ready(function () {
     setBtnFrame($(selectedItem));
   }
 
+  function setBtnFrame(item) {
+    vrDrop.text(item.text());
+    resumeFr.prop("src", `resume-${item.data('date')}/index.html`);
+  }
   $('#vr-drop-btn + .dropdown-menu > .dropdown-item').on('click', function () {
     if (selectedVr !== this.textContent) {
       selectedVr = this.textContent;
       setBtnFrame($(this));
     }
   });
-  function setBtnFrame(item) {
-    vrDrop.text(item.text());
-    resumeFr.prop("src", `resume-${item.data('date')}/index.html`);
-  }
+  // set download
+  let intId = NaN;
+  $('#download-btn').on('click', function () {
+    if (intId) {
+      $(this).popover('dispose');
+      $(this).find('i').each((_, e) => $(e).toggleClass('d-none'));
+      clearInterval(intId);
+      intId = NaN;
+      return;
+    }
+    $(this).popover('show');
+    $(this).find('i').each((_, e) => $(e).toggleClass('d-none'));
+    let count = 3;
+    $("#d-time").text(count);
+    // remember that this timer will take 1000ms to start to initial 1sec delay
+    intId = setInterval(() => {
+      if (count === 1) {
+        $(this).popover('dispose');
+        $(this).find('i').each((_, e) => $(e).toggleClass('d-none'));
+        window.print();
+        clearInterval(intId);
+        intId = NaN;
+        return;
+      }
+      $("#d-time").text(--count);
+    }, 1000);
+  });
 
 });
