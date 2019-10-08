5f2a47ec2132ba74c886eb04902453db2e3232b9
Rajat Parashar <parasharrajat@users.noreply.github.com>
2019-10-08 22:26:54 +0530

    new changes

--- script.js
+++ script.js
@@ -1,38 +1,155 @@
 
 const isChrome = !!window.chrome;
+const versions = [{
+  date: '06-10-2019',
+  title: 'OCT 06, 2019',
+  variant: [{
+    data: 'uk',
+    title: 'UK & Europe',
+  },
+  {
+    data: 'universal',
+    title: 'Universal',
+  }],
+  selected: true
+},
+{
+  date: '10-03-2019',
+  title: 'MAR 10, 2019',
+  variant: [{
+    data: 'universal',
+    title: 'Universal',
+  }]
+},
+
+];
+const polyfills = [
+  ''
+];
+function loadPolyfills() {
+
+}
+
 
 $(document).ready(function () {
 
   // $('[data-toggle="popover"]').popover();
   // load the correct resume
   let selectedVr = "";
-  const vrDrop = $('#vr-drop-btn');
+  let selectedVar = "";
+  const vrDrop = $('.vr-drop');
+  const vrDropBtn = vrDrop.find('#vr-drop-btn');
+  const varDrop = $('.variant-drop');
+  const varDropBtn = varDrop.find('#variant-drop-btn');
   const resumeFr = $('#resume-frame');
-  const vrs = $('#vr-drop-btn + .dropdown-menu > .dropdown-item');
-  const fragement = new window.URL(window.location.href).hash;
-  const hasFrag = fragement && fragement.startsWith(`#resume-`);
 
-  const selectedItem = hasFrag && vrs.toArray().find(e => $(e).data("date") === fragement.split(`#resume-`)[1]) || vrs.toArray().find(e => $(e).data("selected"));
-  if (selectedItem) {
-    selectedVr = $(selectedItem).text();
-    setBtnFrame($(selectedItem));
+
+  createVrDropdown();
+  fillTheFrame();
+
+
+
+  function createVrDropdown() {
+    // variant dropdown
+    const vrDrop = $('.vr-drop > .dropdown-menu');
+    const items = versions.map(ver => {
+      const item = $("<a class='dropdown-item'></a>");
+      item.data({
+        selected: !!ver.selected,
+        date: ver.date
+      });
+      item.attr('href', 'javascript:void(0)');
+      item.text(ver.title);
+      return item;
+    });
+    vrDrop.empty();
+    vrDrop.append(items);
+    attachVersionlistner();
   }
 
-  function setBtnFrame(item) {
-    vrDrop.text(item.text());
-    resumeFr.prop("src", `resume-${item.data('date')}/index.html`);
+  function createVarDropdown(vr) {
+    // variant dropdown
+    const varDrop = $('.variant-drop > .dropdown-menu');
+    const items = vr['variant'].map(varr => {
+      const item = $("<a class='dropdown-item'></a>");
+      item.data({
+        selected: varr.data === 'universal',
+        variant: varr.data
+      });
+      item.attr('href', 'javascript:void(0)');
+      item.text(varr.title);
+      return item;
+    });
+    varDrop.empty();
+    varDrop.append(items);
+    attachVariantlistner();
   }
-  $('#vr-drop-btn + .dropdown-menu > .dropdown-item').on('click', function () {
-    if (selectedVr !== this.textContent) {
-      selectedVr = this.textContent;
-      setBtnFrame($(this));
-    }
-  });
 
-// Toogle download btn
-if(!isChrome){
-  $('#download-btn').remove();
-}
+  function fillTheFrame() {
+    const url = new window.URL(window.location.href);
+    const fragement = url.hash;
+    const params = url.searchParams;
+    const getVr = fragement && fragement.startsWith(`#resume-`) && versions.find(vr => vr["date"] === fragement.split(`#resume-`)[1]) || versions.find(vr => vr["selected"]);
+    const getVar = params && params.has('var') && getVr['variant'].find(vrr => vrr["data"] === params.get('var')) || getVr['variant'].find(vrr => vrr["data"] === 'universal');
+    selectedVr = getVr['date'];
+    selectedVar = getVar['data'];
+    vrDropBtn.text(getVr['title']);
+    varDropBtn.text(getVar['title']);
+    setFrame(getVr['date'], getVar['data']);
+    createVarDropdown(getVr);
+  }
+
+  function setFrame(date, variant) {
+    resumeFr.prop("src", `resume-${date}/${variant !== 'universal' ? variant + '/' : ''}index.html`);
+  }
+
+  function attachVersionlistner() {
+    vrDropBtn.next().find('.dropdown-item').on('click', function () {
+      const version = versions.find(r => r['date'] === $(this).data('date'));
+      createVarDropdown(version);
+      if (selectedVr !== version['date']) {
+        selectedVr = version['date'];
+        vrDropBtn.text(version['title']);
+        const url = new URL(window.location.href);
+        url.hash = `#resume-${version['date']}`;
+        const ifVariant = version['variant'].find(r => r['data'] === selectedVar);
+        if (ifVariant) {
+          varDropBtn.text(ifVariant['title']);
+          url.searchParams.set('var', selectedVar);
+          setFrame(version['date'], selectedVar);
+        } else {
+          varDropBtn.text('Universal');
+          url.searchParams.set('var', 'universal');
+          setFrame(version['date'], 'universal');
+        }
+        history.pushState({}, null, url.href);
+        // window.location.assign(url);
+      }
+    });
+  }
+  function attachVariantlistner() {
+    varDropBtn.next().find('.dropdown-item').on('click', function () {
+
+      const version = versions.find(r => r['date'] === selectedVr);
+      const variant = version['variant'].find(r => r['data'] === $(this).data('variant'));
+
+      if (selectedVar !== variant['data']) {
+        selectedVar = variant['data'];
+        varDropBtn.text(variant['title']);
+        setFrame(version['date'], selectedVar);
+
+        const url = new URL(window.location.href);
+        url.searchParams.set('var', selectedVar);
+        history.pushState({}, null, url.href);
+
+        // window.location.assign(url);
+      }
+    });
+  }
+  // Toogle download btn
+  if (!isChrome) {
+    $('#download-btn').remove();
+  }
 
   // set download
   let intId = NaN;
